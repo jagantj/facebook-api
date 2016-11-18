ae69425c668a018469f405107380ac56ac56754e
jayaramj <Jagannathan.Jayaraman@rabobank.nl>
2016-11-18 11:30:45 +0100

    added key words

--- src/main/java/com/jagan/FacebookPageSearch.java
+++ src/main/java/com/jagan/FacebookPageSearch.java
@@ -4,34 +4,33 @@ import com.google.common.base.Charsets;
 import com.google.common.collect.Lists;
 import com.google.common.io.CharSink;
 import com.google.common.io.Files;
-import com.restfb.Connection;
-import com.restfb.DefaultFacebookClient;
-import com.restfb.FacebookClient;
-import com.restfb.Parameter;
+import com.restfb.*;
 import com.restfb.types.Page;
 import com.restfb.types.Post;
 
 import java.io.File;
 import java.io.IOException;
 
-import java.util.Date;
+
 import java.util.List;
 
 /**
  * Created by Jagan on 07/11/2016.
+ *
+ *
  */
 public class FacebookPageSearch {
 
     public static void main(String[] args) throws IOException {
 
-        FacebookClient.AccessToken accessToken = new DefaultFacebookClient().obtainAppAccessToken("1119322921489035", "7899dd52c9bf14de8d15bedb44c3a8ac");
+        FacebookClient.AccessToken accessToken = new DefaultFacebookClient(Version.VERSION_2_2).obtainAppAccessToken("1119322921489035", "7899dd52c9bf14de8d15bedb44c3a8ac");
 
         FacebookClient fbClient = new DefaultFacebookClient(accessToken.getAccessToken());
         File file = new File("c:/Tools/test.txt");
         List<String> feeds = Lists.newArrayList();
 
         /** Search Page **/
-        Connection<Page> pageResults =  fbClient.fetchConnection("search", Page.class, Parameter.with("q", "Rabobank Netherlands"),
+        Connection<Page> pageResults =  fbClient.fetchConnection("search", Page.class, Parameter.with("q", "Rabobank"),
                 Parameter.with("type", "page"));
 
         int noOfPagesSearched = 0;
@@ -44,24 +43,26 @@ public class FacebookPageSearch {
                     Connection<Post> postFeed = fbClient.fetchConnection(page.getId()+"/feed", Post.class);
                      for (List<Post> posts : postFeed) {
                         // System.out.println(posts.size());
-                         if(noOfPosts > 2) {
+                         if(noOfPosts > 15) {
                              break;
                          }
                          int noOfPost = 0;
                          for (Post post : posts) {
-                             if(noOfPost > 2) {
+                             if(noOfPost > 15) {
                                  break;
                              }
 
-                             System.out.println(post.getCreatedTime()
-                                     + " " + page.getName() + " " + post.getMessage() );
-                             feeds.add(post.getCreatedTime().toString());
-                             feeds.add(page.getName());
-                             feeds.add(post.getMessage());
-                             feeds.add(System.getProperty("line.separator"));
 
+                             if(post!= null  && post.getMessage()!=null && post.getMessage().contains("mobiel bankieren") ) {
+                                 System.out.println(post.getCreatedTime()
+                                         + " " + page.getName() + " " + post.getMessage() );
+                                 feeds.add(post.getCreatedTime().toString());
+                                 feeds.add(page.getName());
+                                 feeds.add(post.getMessage());
+                                 feeds.add(System.getProperty("line.separator"));
+                                 noOfPost ++;
+                             }
 
-                             noOfPost ++;
                           }
                          noOfPosts ++;
                      }
